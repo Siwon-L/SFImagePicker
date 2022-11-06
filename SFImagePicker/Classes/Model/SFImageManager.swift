6b618ddc0ed5683d58111f35954cd7033d03c18c
saafaaari <111dltldnjs@naver.com>
2022-11-06 13:30:45 +0900

    feat: SFImageManager Model 구현 #1

---
+++ SFImagePicker/Classes/Model/SFImageManager.swift
@@ -0,0 +1,37 @@
+//
+//  SFImageManager.swift
+//  SFImagePicker
+//
+//  Created by 이시원 on 2022/11/06.
+//
+
+import Photos
+import UIKit
+
+public final class SFImageManager {
+  private let asset: PHAsset
+  private let manager: PHCachingImageManager
+  private let fatchOptions: PHImageRequestOptions
+  
+  init(
+    asset: PHAsset,
+    manager: PHCachingImageManager,
+    fatchOptions: PHImageRequestOptions
+  ) {
+    self.asset = asset
+    self.manager = manager
+    self.fatchOptions = fatchOptions
+  }
+  
+  public func request(
+    size: CGSize,
+    completion: @escaping (UIImage?, [AnyHashable : Any]?) -> Void
+  ) {
+    manager.requestImage(
+      for: asset,
+      targetSize: size,
+      contentMode: .aspectFill,
+      options: fatchOptions,
+      resultHandler: completion)
+  }
+}
