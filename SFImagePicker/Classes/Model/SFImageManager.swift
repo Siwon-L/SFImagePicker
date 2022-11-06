06f38f25f8111b7d0717d3b6486de501246dba0a
saafaaari <111dltldnjs@naver.com>
2022-11-06 16:39:19 +0900

    feat: SFImageManager assetID 프로퍼티 추가 #1

--- SFImagePicker/Classes/Model/SFImageManager.swift
+++ SFImagePicker/Classes/Model/SFImageManager.swift
@@ -10,28 +10,32 @@ import UIKit
 
 public final class SFImageManager {
   private let asset: PHAsset
+  private let assetID: UUID
   private let manager: PHCachingImageManager
   private let fatchOptions: PHImageRequestOptions?
   
   init(
     asset: PHAsset,
+    assetID: UUID = UUID(),
     manager: PHCachingImageManager,
-    fatchOptions: PHImageRequestOptions?
-  ) {
-    self.asset = asset
-    self.manager = manager
-    self.fatchOptions = fatchOptions
-  }
+    fatchOptions: PHImageRequestOptions?) {
+      self.asset = asset
+      self.assetID = assetID
+      self.manager = manager
+      self.fatchOptions = fatchOptions
+    }
   
   public func request(
     size: CGSize,
-    completion: @escaping (UIImage?, [AnyHashable : Any]?) -> Void
+    completion: @escaping (UIImage?, UUID?) -> Void
   ) {
     manager.requestImage(
       for: asset,
       targetSize: size,
       contentMode: .aspectFill,
-      options: fatchOptions,
-      resultHandler: completion)
+      options: fatchOptions
+    ) { [weak self] image, _ in
+      completion(image, self?.assetID)
+    }
   }
 }
