ea1b0607e90caf169fa1c93510c783685d324972
saafaaari <111dltldnjs@naver.com>
2022-11-07 15:19:29 +0900

    feat: UICollectionViewCell identifier 구현 #3

---
+++ SFImagePicker/Classes/Extension/UICollectionViewCell+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewCell+Extension.swift
+//  Pods-SFImagePicker_Example
+//
+//  Created by 이시원 on 2022/11/07.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+  static var identifier: String {
+    return String(describing: self)
+  }
+}
