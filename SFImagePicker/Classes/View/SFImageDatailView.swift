d6e9a406aa128dedbebd0b9dc264a3cbff08780a
Safari <111dltldnjs@gmail.com>
2023-07-11 16:37:53 +0900

    feat: SFImageDatailView 구현

--- SFImagePicker/Classes/View/SFImageDatailView.swift
+++ SFImagePicker/Classes/View/SFImageDatailView.swift
@@ -5,4 +5,59 @@
 //  Created by 이시원 on 2023/07/11.
 //
 
-import Foundation
+import UIKit
+
+final class SFImageDatailView: UIView {
+  private(set) lazy var imageCollectionView: UICollectionView = {
+    let collectionView = UICollectionView(
+      frame: .zero,
+      collectionViewLayout: collectionViewLayout 
+    )
+    collectionView.isScrollEnabled = false
+    return collectionView
+  }()
+  
+  init() {
+    super.init(frame: .zero)
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configureUI() {
+    addSubview(imageCollectionView)
+    imageCollectionView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      imageCollectionView.topAnchor.constraint(equalTo: self.topAnchor),
+      imageCollectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+      imageCollectionView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+      imageCollectionView.widthAnchor.constraint(equalTo: self.widthAnchor)
+    ])
+  }
+  
+}
+
+extension SFImageDatailView {
+  private var collectionViewLayout: UICollectionViewCompositionalLayout {
+    let itemSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalWidth(1),
+      heightDimension: .fractionalHeight(1)
+    )
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+    
+    let groupSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalHeight(1),
+      heightDimension: .fractionalHeight(1)
+    )
+    let group = NSCollectionLayoutGroup.horizontal(
+      layoutSize: groupSize,
+      subitems: [item]
+    )
+    
+    let section = NSCollectionLayoutSection(group: group)
+    section.orthogonalScrollingBehavior = .paging
+    return UICollectionViewCompositionalLayout(section: section)
+  }
+}
